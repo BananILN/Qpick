c0edf4c4fa0b8a54e3a39f56c555ce4ba4e74b5e
BananILN <daniil.klovans84@mail.ru>
2025-04-19 18:25:18 +0400

    add data

---
+++ Qpick/src/data.ts
@@ -0,0 +1,50 @@
+interface Headphone {
+    img:string;
+    title: string;
+    price:number;
+    rate: number;
+}
+
+
+const headphones: Headphone[] = [
+    {
+        img: "/src/assets/cardImg/appleByz.png",
+        title: "Apple BYZ S852I",
+        price: 2927,
+        rate: 4.7,
+    },
+    {
+        img: "/src/assets/cardImg/AppleEarpods.png",
+        title: "Apple BYZ S852I",
+        price: 2327 ,
+        rate: 4.5,
+    },
+    {
+        img: "/src/assets/cardImg/AppleErpos2.png",
+        title: "Apple BYZ S852I",
+        price: 2327,
+        rate: 4.5,
+    },
+] 
+
+const WirelessHeadPhones: Headphone[] =[
+    {
+        img: "/src/assets/cardImg/appleAitPod.png",
+        title: "Apple BYZ S852I",
+        price: 9527,
+        rate: 4.7,
+    },
+    {
+        img: "/src/assets/cardImg/Gergax.png",
+        title: "Apple BYZ S852I",
+        price: 6527,
+        rate: 4.7,
+    },
+    {
+        img: "/src/assets/cardImg/Borophone.png",
+        title: "Apple BYZ S852I",
+        price: 7527,
+        rate: 4.7,
+    },
+    
+]
